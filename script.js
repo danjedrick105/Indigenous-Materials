adc6ac0922286b1b5e0ed6be97f22945d7d05ac8
Dan <dandeleon52023@gmail.com>
2026-02-20 23:21:11 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ function hideSidebar(){
     const Sidebar = document.querySelector('.sidebar')
     Sidebar.style.display = 'none'
 }
+
+const navbar = document.querySelector("nav");
+    const navOffset = navbar.offsetTop;
+
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > navOffset) {
+            navbar.classList.add("sticky");
+        } else {
+            navbar.classList.remove("sticky");
+        }
+    });
